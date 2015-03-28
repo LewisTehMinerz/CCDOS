7e1fdb3056c6511f89c3ebc2caea5806b8b65cbc
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-28 15:11:41 +0000

    Create command.lua

---
+++ command.lua
@@ -0,0 +1,9 @@
+_G.CCDOS = {
+  [ "PARTITION" ] = {
+    [ "MASTER" ]  = {
+      [ "PRIMARY_DOS_PARTITION" ] = true,
+    },
+  },
+}
+
+if not fs.exists( "PARTITIONS/MASTER/PRIMARY_DOS_PARTITION/DOS" ) then fs.create( "/PARTITIONS/MASTER/PRIMARY_DOS_PARTITION/DOS" ) end
