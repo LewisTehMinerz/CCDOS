10514f67f5368cd1d523c991890327a9cc484dee
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-28 15:22:02 +0000

    Update AUTO.lua

--- AUTO.lua
+++ AUTO.lua
@@ -2,4 +2,5 @@ print( "CC-DOS for ComputerCraft starting..." )
 print()
 print()
 print( "CC-DOS is checking environment..." )
-if not fs or not shell or not term then CCDOS.ERROR( "Invalid Environment" )
+if not fs or not shell or not term then CCDOS.ERROR( "Invalid Environment" ) end
+print( "Checks passed successfully. Continuing to run command.lua..." )
