783207e965c7a30a06acdd272e539683e80b482e
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-28 15:25:12 +0000

    Update partitionmanager.lua

--- partitionmanager.lua
+++ partitionmanager.lua
@@ -11,3 +11,10 @@ function checkPartition( name )
     return false
   end
 end
+function gotoPartition( name )
+  if not fs.exists( "Partitions/" .. name  ) then
+    error( "CC-DOS PARTITION MANAGER ERROR >>> Invalid partition was selected when trying to goto partition \"" .. name .. "\".")
+  else
+    shell.setDir( "Partitions/" .. name )
+  end
+end
