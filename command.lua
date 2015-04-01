83c380e66fead9c692710b839b55ac2b9d6b49bc
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-04-01 10:42:37 +0100

    Update command.lua

--- command.lua
+++ command.lua
@@ -2,11 +2,12 @@ _G.CCDOS = {
   [ "ERROR" ] = function( err )
     error( "CC-DOS ERROR >>> " .. err, 0 )
   end,
+  [ "IMPORT_FILE" ] = function( f )
+    os.loadAPI( f )
+  end,
 }
 
-
-
-
+CCDOS.IMPORT_FILE( "partitionmanager" )
 
 if not partitionmanager.partitionExists( "DOS" ) then
   partitionmanager.createPartition( "DOS" )
